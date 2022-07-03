314c63396f01709e59beee683aaaf8369731b2ad
DmitriyChebruchan <chebruchan2707@gmail.com>
2022-07-03 22:40:54 +0300

    names of additional files were update

--- page_loader/additionals/replacers.py
+++ page_loader/additionals/replacers.py
@@ -9,29 +9,6 @@ def replace_links(file_name, combined_dict, dict_of_files):
             replace_href_of_element(file_name, lists[0], dict_of_files[key])
 
 
-def url_to_file_name(url):
-    url = url.replace('https://', '')
-    url = url.replace('http://', '')
-    url = url.replace('.', '-') + '.html'
-    url = url.replace('/', '-')
-    return url
-
-
-def replaced_src(new_src, old_src, soup, file_format):
-    options = {'imgs': 'img',
-               'scripts': 'script',
-               'link': 'link'}
-    tag = soup.select(options.get(file_format) + '[src="' + old_src + '"]')
-    tag[0]['src'] = new_src
-    return soup
-
-
-def replaced_href(new_src, old_src, soup):
-    tag = soup.select('link[href="' + old_src + '"]')
-    tag[0]['href'] = new_src
-    return soup
-
-
 def replace_src_of_element(file_name, updated_files_list_names,
                            old_img_list_names, key):
     with open(file_name, 'r') as f:
@@ -46,6 +23,15 @@ def replace_src_of_element(file_name, updated_files_list_names,
         output_file.write(str(soup))
 
 
+def replaced_src(new_src, old_src, soup, file_format):
+    options = {'imgs': 'img',
+               'scripts': 'script',
+               'link': 'link'}
+    tag = soup.select(options.get(file_format) + '[src="' + old_src + '"]')
+    tag[0]['src'] = new_src
+    return soup
+
+
 def replace_href_of_element(file_name, updated_files_list_names,
                             old_img_list_names):
     with open(file_name, 'r') as f:
@@ -58,3 +44,17 @@ def replace_href_of_element(file_name, updated_files_list_names,
 
     with open(file_name, 'w') as output_file:
         output_file.write(str(soup))
+
+
+def replaced_href(new_src, old_src, soup):
+    tag = soup.select('link[href="' + old_src + '"]')
+    tag[0]['href'] = new_src
+    return soup
+
+
+def url_to_file_name(url):
+    url = url.replace('https://', '')
+    url = url.replace('http://', '')
+    url = url.replace('/', '-')
+    url = url.replace('.', '-') + '.html'
+    return url
